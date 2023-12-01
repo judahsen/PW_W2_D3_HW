5dd371d5f63f0d5ed9e8c199e4c0e7625d534444
judahsen <sardiusjudah@gmail.com>
2023-11-30 19:11:06 -0600

    first commit

---
+++ homeworkmd.py
@@ -0,0 +1,10 @@
+def Sqft_calc(L, W):
+    #Length X Width = Area
+    L = input('Enter Length = ')
+    W = input('Enter Width = ')
+    print(L* W)
+
+
+def Sqft_calc(L, W):
+    #Length X Width = Area
+       print(L* W)
