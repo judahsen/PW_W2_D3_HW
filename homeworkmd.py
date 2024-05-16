facf1ca325c4c63b69607cd1b23cfdb58ec38c60
judahsen <sardiusjudah@gmail.com>
2024-05-16 00:55:22 -0500

    new commit

--- homeworkmd.py
+++ homeworkmd.py
@@ -1,5 +1,15 @@
 
-def Sqft_calc(L, W):
-    #Length X Width = Area
-       print("Area =",L* W)
 
+def Sqft_calc(length, width):
+    if length <= 0 or width <= 0:
+        raise ValueError("Length and width must be positive numbers.")
+    
+    return length * width
+
+def calculate_circumference(radius):
+    
+    if radius <= 0:
+        raise ValueError("Radius must be a positive number.")
+    
+    pi = 3.141592653589793
+    return 2 * pi * radius
